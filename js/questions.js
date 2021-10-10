b6b893a0f047c92a11ea9c85b2305958963789d8
Ganesh C L <44645893+Jambuganesh@users.noreply.github.com>
2021-10-11 00:05:04 +0530

    Add initial files

---
+++ js/questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following work is done by family members??",
+    answer: "All of these",
+    options: [
+      "Buying things",
+      "Sweeping",
+      "Cleaning Utensils",
+      "All of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which part of a plant does give vegetables?",
+    answer: "All of these",
+    options: [
+      "All of these",
+      "Roots ",
+      "Stem",
+      "Leaves"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is used as medicines in cough and cold?",
+    answer: "All of these",
+    options: [
+      "Ginger",
+      "Amla",
+      "Tulsi",
+      "All of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is/are cereals?",
+    answer: "Maize",
+    options: [
+      "Chillies",
+      "Maize",
+      "All of these",
+      "Maida"
+    ]
+  },
+    {
+    numb: 5,
+    question: "We get food from_________",
+    answer: "Both of them",
+    options: [
+      "Plants",
+      "Animals",
+      "Both of them",
+      "None of them"
+    ]
+  },
+    {
+     numb: 6,
+     question: "Which part of a plant is underground?",
+     answer: "Root",
+     options: [
+       "Stem",
+       "Root",
+       "Fruit",
+       "Leaves"
+     ]
+  },
+  {
+    numb: 7,
+    question: "Which part of a plant gives sweet potato?",
+    answer: "Root",
+    options: [
+      "Stem",
+      "Root",
+      "Fruit",
+      "Leaves"
+    ]
+ },  
+];
